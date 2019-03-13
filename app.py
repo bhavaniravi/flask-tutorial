394d6ee487d3eaf09c56ef05492e80f7649216ef
Bhavani Ravi <bhava0895@gmail.com>
2019-03-14 00:59:44 +0530

    add view functions

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request
+from service import ToDoService
 app = Flask(__name__)
 
 
@@ -10,6 +11,27 @@ def hello():
 @app.route("/<name>")
 def hello_name(name):
     return "Hello " + name
+
+
+@app.route("/todo", method=["GET"])
+def list_todo():
+    return ToDoService().list_todo()
+
+
+@app.route("/todo", method=["POST"])
+def create_todo():
+    return ToDoService().create_to(request.get_json())
+
+
+@app.route("/todo/<item_id>", method=["PUT"])
+def update_item(item_id):
+    return ToDoService().update(item_id, request.get_json())
+
+
+@app.route("/todo/<item_id>", method=["DELETE"])
+def delete_item(item_id):
+    return ToDoService().delete(item_id)
+
 
 
 if __name__ == "__main__":
