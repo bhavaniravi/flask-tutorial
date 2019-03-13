1c3a05c4732945c615bfff47405c6065372f76ee
Bhavani Ravi <bhava0895@gmail.com>
2019-03-13 21:21:52 +0530

    hello world app

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
+@app.route("/<name>")
+def hello_name(name):
+    return "Hello " + name
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
