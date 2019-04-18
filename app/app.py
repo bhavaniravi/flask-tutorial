91a88b2786ab7a7a468ff2783c50259eac881c70
Bhavani Ravi <bhava0895@gmail.com>
2019-04-18 08:33:07 +0530

    modify path of files and add access control

--- app/app.py
+++ app/app.py
@@ -7,6 +7,14 @@ import json
 app = Flask(__name__)
 
 
+@app.after_request
+def add_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Headers'] =  "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"
+    response.headers['Access-Control-Allow-Methods']=  "POST, GET, PUT, DELETE, OPTIONS"
+    return response
+
+
 @app.route("/")
 def hello():
     return "Hello World!"
@@ -27,16 +35,16 @@ def create_todo():
     return jsonify(ToDoService().create(request.get_json()))
 
 
-@app.route("/todo/<item_id>", methods=["PUT"])
+@app.route("/todo/<item_id>", methods=["PU"])
 def update_item(item_id):
-    return ToDoService().update(item_id, request.get_json())
+    return jsonify(ToDoService().update(item_id, request.get_json()))
 
 
 @app.route("/todo/<item_id>", methods=["DELETE"])
 def delete_item(item_id):
-    return ToDoService().delete(item_id)
+    return jsonify(ToDoService().delete(item_id))
 
 
 if __name__ == "__main__":
     Schema()
-    app.run(debug=True)
+    app.run(debug=True, port=8888)
